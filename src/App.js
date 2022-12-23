0a6e73af3a9ff5183d9473535827ec22699e87b9
Russell Elliott <russell@quadsquad.net>
2022-12-23 10:41:09 -0800

    Made a basic wireframe of site pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import About from "./components/pages/About";
+import Hacker from "./components/pages/Hacker";
+import Nav from "./components/navbar/Nav";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" exact element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/hacker" element={<Hacker/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h1>Home page</h1>;
+}
---
+++ src/components/pages/About.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "../../App.css";
+
+export default function About() {
+  return <div>About page</div>;
+}
